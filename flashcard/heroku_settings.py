d15702568e73abf52b8ff3b52e68f35e72dcbfd8
faxedexcel <chinh.le23@gmail.com>
2019-04-02 16:42:11 -0400

    settings file updated for Heroku

--- flashcard/heroku_settings.py
+++ flashcard/heroku_settings.py
@@ -1,6 +1,6 @@
 from flashcard.settings import *
 import django_heroku
 
-DEBUG = True
+DEBUG = False
 
 django_heroku.settings(locals())
--- flashcard/settings.py
+++ flashcard/settings.py
@@ -137,3 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
